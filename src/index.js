7e3f898def880dcfaf8c9e3e9279729fb597f0c1
Cristian <cristian.bilert@celk.net>
2020-07-28 20:58:44 -0300

    Finalizando slider videos aula 2

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './reset.css';
+import { 
+  BrowserRouter,
+  Switch,
+  Route
+} from 'react-router-dom';
+
+import CadastroVideo from './pages/cadastro/Video';
+import Pagina404 from './pages/pag404';
+import Home from './pages/Home';
+import CadastroCategoria from './pages/cadastro/Categoria';
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Home} exact/>
+      <Route path="/cadastro/Video" component={CadastroVideo}/>
+      <Route path="/cadastro/Categoria" component={CadastroCategoria}/>
+      <Route component={Pagina404} />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
---
+++ src/pages/pag404.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+function Pagina404 () {
+    return (
+        <div> Pagina 404</div>
+    );
+}
+
+export default Pagina404;
