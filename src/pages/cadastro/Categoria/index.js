6d9ab8dbba39b4a25bf2002ca600ced18efec907
Cristian <cristian.bilert@celk.net>
2020-07-31 21:14:36 -0300

    corrigindo load dados dev/producao

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -29,22 +29,22 @@ function CadastroCategoria() {
   }
 
   useEffect(() => {
-    if (window.location.href.includes('localhost')) {
-      const URL = 'https://agroflix.herokuapp.com/categorias';
-      console.log(URL);
-      fetch(URL)
-        .then(async (respostaDoServer) => {
-          if (respostaDoServer.ok) {
-            const resposta = await respostaDoServer.json();
-            console.log(resposta);
-            setCategorias([
-              ...resposta,
-            ]);
-            return;
-          }
-          throw new Error('Não foi possível pegar os dados');
-        });
-    }
+    //if (window.location.href.includes('localhost')) {
+    const URL = window.location.href.includes('localhost') ? 'http://localhost:8080/categorias' : 'https://agroflix.herokuapp.com/categorias';
+    console.log(URL);
+    fetch(URL)
+      .then(async (respostaDoServer) => {
+        if (respostaDoServer.ok) {
+          const resposta = await respostaDoServer.json();
+          console.log(resposta);
+          setCategorias([
+            ...resposta,
+          ]);
+          return;
+        }
+        throw new Error('Não foi possível pegar os dados');
+      });
+    //}
   }, []);
 
   return (
